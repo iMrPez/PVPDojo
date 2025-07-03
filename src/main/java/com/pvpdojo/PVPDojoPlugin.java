8a7794e34ade8e2a9ed90fd51d777a4c6d25d1b1
Branden Weber <brandenweber1@gmail.com>
2025-07-03 15:38:06 -0400

    0.10 - Initial Version
    - Implemented Basic Combat System.
    - Setup 13 Weapons.
    - Setup Basic Replacements for Special Attacks, Eating/Drinking, Prayer.

--- src/main/java/com/pvpdojo/PVPDojoPlugin.java
+++ src/main/java/com/pvpdojo/PVPDojoPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.pvpdojo;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,7 +14,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "PVP Dojo"
 )
 public class ExamplePlugin extends Plugin
 {
--- src/test/java/com/pvpdojo/PluginLauncher.java
+++ src/test/java/com/pvpdojo/PluginLauncher.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.pvpdojo;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
